713a5ce4a392ceb8325eab295b7f772c4b0597fe
huanyushi <1174601344@qq.com>
2024-10-29 20:55:46 +0800

    publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL, // 使用环境变量来设置基础路径
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+    },
+  };
+});
